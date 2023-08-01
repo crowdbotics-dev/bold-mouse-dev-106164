d30ec40ee27db7ad79daae9fc4a16cd8deac193e
Crowdbotics <engineering@crowdbotics.com>
2023-08-01 06:52:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bold-mouse-dev-106164",
+  url: "https://bold-mouse-dev-106164.botics.co",
+  api: "https://bold-mouse-dev-106164.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
